33460e9166b07254b0241530cb9851105e1a5b84
makennamartin97 <makennamartin97@yahoo.com>
2020-09-04 16:55:29 -0400

    update for domain name

--- src/App.js
+++ src/App.js
@@ -11,17 +11,17 @@ function App() {
   return (
     <div>
       <nav className="navbar navbar-expand-lg navbar-light fixed-top">
-        <a className="navbar-brand text-light m-2 lead" href="/final-portfolio3#top">Makenna Martin</a>
+        <a className="navbar-brand text-light m-2 lead" href="/#top">Makenna Martin</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="collapsibleNavbar">
           <div className="navbar-nav">
             <li className="nav-item">
-              <a className="nav-link text-light lead" href="/final-portfolio3#projects">Projects</a>
+              <a className="nav-link text-light lead" href="/#projects">Projects</a>
             </li>
             <li className="nav-item">
-              <Link className="nav-link text-light lead" to="/final-portfolio3/resume">Resume</Link>
+              <Link className="nav-link text-light lead" to="/resume">Resume</Link>
             </li>
             <li className="nav-item">
               <a className="nav-link text-light lead active" href="https://www.linkedin.com/in/makennamartin97/">LinkedIn</a>
@@ -30,15 +30,15 @@ function App() {
               <a className="nav-link text-light lead active" href="https://github.com/makennamartin97">Github</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link text-light lead" href="/final-portfolio3#contact">Contact</a>
+              <a className="nav-link text-light lead" href="/#contact">Contact</a>
             </li>
 
           </div>
         </div>
       </nav>
       <Router>
-        <Mainpage path="/final-portfolio3"/>
-        <Resume path="/final-portfolio3/resume"/>
+        <Mainpage path="/"/>
+        <Resume path="/resume"/>
 
 
       </Router>
